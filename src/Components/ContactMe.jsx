55d4eb1e1db0465619616cfe68848abf408102fe
usersyedsaad <saadsohail360@gmail.com>
2025-01-15 06:20:34 +0400

    Footer Done

--- src/Components/ContactMe.jsx
+++ src/Components/ContactMe.jsx
@@ -6,14 +6,14 @@ const ContactMe = () => {
     <div className='text-white  pt-[256px] flex flex-col items-center justify-between gap-[64px]'>
       
       <div className='lets-make-etc flex flex-col self-end gap-[24px] w-[76%]' >
-        <h6 className='text-white font-[300] leading-normal tracking-[-2.24px] text-[56px] text-left '>Let’s Make Something <span className='text-[#01B7C3]'>Great Together</span>. Start a <span className='text-[#01B7C3]'>conversation</span> 👋</h6>
-        <p className='text-[#A4A7AC] text-[24px] leading-normal tracking-[-0.48px]'>Get in touch if you want to discuss your project , learn more about my methodologies, or just to say hello!</p>
+        <h6 className='text-white font-[300] leading-normal tracking-[-2.24px] text-3xl md:text-4xl lg:text-5xl xl:text-[56px] text-left '>Let’s Make Something <span className='text-[#01B7C3]'>Great Together</span>. Start a <span className='text-[#01B7C3]'>conversation</span> 👋</h6>
+        <p className='text-[#A4A7AC] text-base md:text-lg lg:text-xl xl:text-[24px] leading-normal tracking-[-0.48px]'>Get in touch if you want to discuss your project , learn more about my methodologies, or just to say hello!</p>
       <div className='social-links self-start pt-[64px] flex gap-[40px]'>
         <Button buttonTitle="HMTalha@gmail.com" style={{borderRadius: '32px', backgroundColor: '#01B7C3', color:'#1B2230'}}/>
         <span className='flex gap-[8px]'>
-        <Button buttonTitle="Linkedin" style={{background: 'rgba(255, 255, 255, 0.10)', border: '1px solid rgba(255, 255, 255, 0.20)' ,borderRadius: '32px'}}/>
-        <Button buttonTitle="Email" style={{background: 'rgba(255, 255, 255, 0.10)', border: '1px solid rgba(255, 255, 255, 0.20)', borderRadius: '32px'}}/>
-        <Button buttonTitle="Whatsapp" style={{background: 'rgba(255, 255, 255, 0.10)', border: '1px solid rgba(255, 255, 255, 0.20)' ,borderRadius: '32px'}}/>
+        <Button className="xl:text-[24px] lg:text-xl md:text-lg text-base" buttonTitle="Linkedin" style={{background: 'rgba(255, 255, 255, 0.10)', border: '1px solid rgba(255, 255, 255, 0.20)' ,borderRadius: '32px'}}/>
+        <Button className="xl:text-[24px] lg:text-xl md:text-lg text-base" buttonTitle="Email" style={{background: 'rgba(255, 255, 255, 0.10)', border: '1px solid rgba(255, 255, 255, 0.20)', borderRadius: '32px'}}/>
+        <Button className="xl:text-[24px] lg:text-xl md:text-lg text-base" buttonTitle="Whatsapp" style={{background: 'rgba(255, 255, 255, 0.10)', border: '1px solid rgba(255, 255, 255, 0.20)' ,borderRadius: '32px'}}/>
         </span>
       </div>
       </div>
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,9 +4,9 @@ import ArrowButton from './Subcomponents/Icons/ArrowButton'
 const Footer = () => {
   return (
     <footer className='text-white  w-full h-auto flex justify-between items-center'>
-      <div className='text-white text-lg font-[300] leading-normal '>© 2022 MH TALHA. All right reserved.</div>
+      <div className='text-white text-xs lg:text-base xl:text-lg font-[300] leading-normal '>© 2022 MH TALHA. All right reserved.</div>
       <div className='flex justify-between items-center w-[217px]'>
-        <p>Back to top</p>
+        <p className='text-sm md:text-base lg:text-lg'>Back to top</p>
         <ArrowButton
         size="60px"
         arrowScale={0.5} // Arrow is 50% of the button size
